15725522649616da9a9543736554ed1dd519d161
Кирилл Абрамов <work.kirillabramov@gmail.com>
2019-02-13 15:46:42 +0200

    Updates

---
+++ precache-manifest.4f09d8a9d704c22e70aaa2758c6155ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "dadb650c3ddc6648a34d220a2d2a1cfc",
+    "url": "/space-stars/static/media/bg.dadb650c.jpg"
+  },
+  {
+    "revision": "f37032b03d4945c0b8b0",
+    "url": "/space-stars/static/js/runtime~main.f37032b0.js"
+  },
+  {
+    "revision": "c44bce22129ab98195c0",
+    "url": "/space-stars/static/js/main.c44bce22.chunk.js"
+  },
+  {
+    "revision": "07cb89c18c999d1356c6",
+    "url": "/space-stars/static/js/1.07cb89c1.chunk.js"
+  },
+  {
+    "revision": "c44bce22129ab98195c0",
+    "url": "/space-stars/static/css/main.6f5c45e4.chunk.css"
+  },
+  {
+    "revision": "c1bf7bd3c36274389a28bfcc014b566d",
+    "url": "/space-stars/index.html"
+  }
+];
